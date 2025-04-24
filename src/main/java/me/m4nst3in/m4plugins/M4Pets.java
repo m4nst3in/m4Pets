6a1c5437670cf0881dc47809aec5fedef8f86410
m4nst3in <m4nsdev@proton.me>
2025-04-24 12:27:07 -0300

    initialize m4Pets plugin with Gradle setup and basic structure

---
+++ src/main/java/me/m4nst3in/m4plugins/M4Pets.java
@@ -0,0 +1,17 @@
+package me.m4nst3in.m4plugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class M4Pets extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
